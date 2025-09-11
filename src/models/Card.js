e798bc951d8366cc684d487fb35305667887932d
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-11 22:20:59 +0530

    refactor: reduce the indentation as per other files.

--- src/models/Card.js
+++ src/models/Card.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
 const cardSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    question: {
-        type: String,
-        required: true
-    },
-    answer: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true,
-        enum: ["Kanji", "Vocabulary"]
-    }
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  question: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["Kanji", "Vocabulary"]
+  }
 });
 
 const Card = mongoose.model("Card", cardSchema);
