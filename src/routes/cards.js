1e2411561242c525eff19208bdf8f880487abb17
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-12 21:14:21 +0530

    feat: add card router

---
+++ src/routes/cards.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import { cards, kanji, vocabulary, star } from "../controllers/Cards.controller";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const router = express.Router();
+
+router.get("/:id", asyncHandler(cards));
+router.get("/kanji", asyncHandler(kanji));
+router.get("/vocabulary", asyncHandler(vocabulary));
+router.post("/star/:id", asyncHandler(star));
+router.delete("/star/:id", asyncHandler(star));
+
+export default router;
