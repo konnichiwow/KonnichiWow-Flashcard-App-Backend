47c52dfb545fbfc7d98e96cd62d87b7147ce2608
arnav <arnav.mathur.1024@gmail.com>
2025-09-12 10:56:58 +0530

    feat: Login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,22 +1,16 @@
 import { auth } from "../config/firebase.js";
+import axios from "axios";
 import User from "../models/user.js";
 
-/**
- * Create a new user
- * Steps:
- * 1. Validate input
- * 2. Check if user exists in MongoDB
- * 3. Create user in Firebase Auth
- * 4. Save minimal user info in MongoDB
- * 5. Return success response
- */
 export const createUser = async (req, res) => {
+  /* This controller is used by the register route.*/
+  /* This controller expects email , password and name in the body*/
   try {
     const{ email, password, name}= req.body;
     if (!email || !password || !name) {
       return res.status(400).json({ error: "Email, password and name are required" });
     }
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email:email });
     if (existingUser) {
       return res.status(400).json({ error: "User already exists" });
     }
@@ -46,7 +40,65 @@ export const createUser = async (req, res) => {
 
   } 
   catch(e){
-    console.error("Error creating user:", e);
+    console.log(`Error creating user: ${e}`);
     res.status(500).json({ error: "Server error" });
   }
 };
+
+export const signInUser = async (req,res) => {
+  try{
+    const {email, password} = req.body;
+    //make checks if data is given properly
+    if(!email){
+      return res.status(400).json({error : "No email received"});
+    }
+    if(!password){
+      return res.status(400).json({error : "No password received"});
+    }
+    //query our db to see if this user exists or not
+    const user = await User.findOne({email:email});
+    if(!user){
+      return res.status(404).json({error : "User email not found. Please signup first"});
+    }
+
+    //sign the user in
+    const firebaseRes = await axios.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`,
+      { email, password, returnSecureToken: true }
+    );
+
+    const {idToken,refreshToken,expiresIn} = firebaseRes.data;
+
+    //now lets set the cookies
+
+     res.cookie("accessToken",idToken,{
+      httpOnly: true,
+      secure: false, //for now
+      sameSite: "strict",
+      maxAge: parseInt(expiresIn) * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, //refresh token remains valid on client for 7 days
+    });
+
+    return res.status(200).json({ message : "Login succesful" , user : {email : user.email, name : user.name}});
+  }
+  catch(e){
+    const err = e.response?.data?.error?.message;
+
+    if(err === "INVALID_LOGIN_CREDENTIALS"){
+      //most common error to be expected
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    if(err === "USER_DISABLED"){
+      return res.status(403).json({ error: "User account disabled" });
+    }
+
+    console.log(`Error in signing in user :`,JSON.stringify(e.response?.data, null, 2) || e.message);
+    return res.status(500).json({ error: "Server Error" });
+  }
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
-import { createUser } from "../controllers/auth.js";
+import { createUser , signInUser} from "../controllers/auth.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post('/signUp', createUser);
+router.post('/register', createUser);
+router.post('/login', signInUser);
 
 export default router;
