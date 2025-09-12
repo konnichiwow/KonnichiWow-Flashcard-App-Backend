5904c859b99b14f02d7205804e8e00c8ea1e5971
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-12 22:30:07 +0530

    feat: use the Fisher-Yates algorithm to shuffle the Cards

--- src/controllers/Cards.controller.js
+++ src/controllers/Cards.controller.js
@@ -2,6 +2,10 @@ import Card from "../models/Card.js";
 import Users from "../models/Users";
 
 const shuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
   return arr;
 };
 
