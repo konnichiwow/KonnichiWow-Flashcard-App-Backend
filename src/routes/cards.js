ebaf1fcd43c8ac267019d0fcb0404b7ad214b910
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-12 22:58:51 +0530

    fix: keep /:id at the end to match it at last

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -5,10 +5,10 @@ import asyncHandler from "../utils/asyncHandler.js";
 
 const router = express.Router();
 
-router.get("/:id", asyncHandler(cards));
 router.get("/kanji", asyncHandler(kanji));
 router.get("/vocabulary", asyncHandler(vocabulary));
 router.post("/star/:id", asyncHandler(star));
 router.delete("/star/:id", asyncHandler(star));
+router.get("/:id", asyncHandler(cards));
 
 export default router;
