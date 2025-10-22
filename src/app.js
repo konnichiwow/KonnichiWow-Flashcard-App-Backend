e7e1347194da5c290eb024c28e66d04b8f76ce7f
KTKarthick006 <aswintamil@gmail.com>
2025-10-22 16:36:29 +0530

    fix: minor change in nodemailer

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import "./config/firebase.js"; 
+import "./config/firebase.js";
 import "./config/nodemailer.js";
 import auth from "./routes/auth.js";
 import cookieParser from "cookie-parser";
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.use('/api/auth', auth);
+app.use("/api/auth", auth);
 
 console.log(process.env.MONGO_URI);
 
--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,20 +1,27 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 /* node-mailer set up */
 const transporter = nodemailer.createTransport({
-    service: process.env.NODEMAILER_SERVICE, //the service
-    auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASSWORD 
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASSWORD,
+  },
+  tls: { rejectUnauthorized: false },
+  debug: true, // <-- enable debug logs
 });
 
 transporter.verify((error, success) => {
-    if (error) {
-        console.log('Transporter connection error:', error.message);
-    } else {
-        console.log('Transporter is ready to send messages');
-    }
+  if (error) {
+    console.log("Transporter connection error:", error.message);
+  } else {
+    console.log("Transporter is ready to send messages");
+  }
 });
 
 export default transporter;
