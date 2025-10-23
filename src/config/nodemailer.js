2d7e7024d47d037dd55891d61dff680330fc2399
arnav <arnav.mathur.1024@gmail.com>
2025-10-23 12:17:18 +0530

    fix:made nodemailer port to 465 and made secure:true

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -6,8 +6,8 @@ dotenv.config();
 /* node-mailer set up */
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.NODEMAILER_EMAIL,
     pass: process.env.NODEMAILER_PASSWORD,
